9cb4d8009e794810985f55cf4a0a5fe6312472d4
Gabriel756-del <tobilena756@gmail.com>
2026-02-24 20:52:24 -0300

    Tratando idade invalidade.

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -1,12 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 int idade;
-Console.Write("Digite a sua idade: ");
-idade = Convert.ToInt32(Console.ReadLine());
-if (idade >= 18)
+bool idadeValidade = false;
+while(!idadeValidade)
 {
-    Console.WriteLine("Você é maior de idade.");
+    Console.Write("Digite a sua idade: ");
+    try
+    {
+        idade = Convert.ToInt32(Console.ReadLine());
+        idadeValidade = true;
+        if (idade >= 18)
+        {
+            Console.WriteLine("Você é maior de idade.");
+        }
+        else
+        {
+            Console.WriteLine("Você é o menor de idade.");
+        }
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("O valor informado deve ser em algarismos algébricos. NÚMEROS");
+    }
 }
-else
-{
-    Console.WriteLine("Você é o menor de idade.");
-}
+
+
